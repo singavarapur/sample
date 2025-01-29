841cabdca0f2c274ca596e32cd9fe13944d7f0fc
singavarapur <rs28795n@pace.edu>
2025-01-29 12:47:24 -0500

    config chabge

--- myapp/vite.config.js
+++ myapp/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',  // Allow access from any IP
+    port: 5173,        // Default Vite port (or change if needed)
+    strictPort: true,  // Ensures it uses the defined port
+    allowedHosts: [
+      'ec2-18-191-189-84.us-east-2.compute.amazonaws.com', // Add your EC2 public hostname
+      'localhost'
+    ]
+  }
+});
+
